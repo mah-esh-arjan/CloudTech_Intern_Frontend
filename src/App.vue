f540a75e3d5bc7d64bc9c04dc38bc4e86f0b0cbe
Mahesh maharjan <mah.dot98@gmail.com>
2025-02-04 15:18:54 +0545

    Added Composition APi

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,10 @@
-<script>
-export default {
-
-  data() {
-
-    return {
-      name: 'Slim Shady',
-      status: 'active',
-      statusNumber: 1,
-      numbers: ['One', 'To', 'Two'],
-      link: 'https://www.google.com/',
-      message: '',
-      showData: true,
-    }
-  },
-  methods: {
-    toggleStatus() {
-      if (this.status === 'active') {
-        this.status = 'pending';
-      }
-      else if (this.status === 'pending') {
-        this.status = 'inactive';
-      }
-      else {
-        this.status = 'active';
-      }
-    },
-    toggleShowData() {
-      if( this.showData === true){
-      this.showData = false;
-    }
-    else{
-      this.showData = true;
-    }
-    }
-
-  }
-
-}
-
+<script setup>
+import OptionsAPI from './components/optionsAPI.vue';
+import CompositionAPI from './components/CompositionAPI.vue';
 </script>
 
+
 <template>
-  <!-- Checking if else if else -->
-  <h1 v-if="status === 'active'"> {{ name }} is active </h1>
-  <h1 v-else-if="status === 'pending'"> {{ name }} is logging in </h1>
-  <h1 v-else> {{ name }} is inactive </h1>
-
-  <!-- Loop with for like .map in react with key as well -->
-  <ul>
-    <li v-for="number in numbers" :key="number">{{ number }}</li>
-  </ul>
-
-  <!-- one way binding putting reusable variables -->
-  <a v-bind:href="link">Google</a>
-  <a :href="link">:href="link" works</a>
-
-  <!-- Event listerner using on:click -->
-  <button v-on:click="toggleStatus">Change Status from {{ status }}</button>
-  <button @click="toggleStatus">@click also works </button>
-
-
-  <div>
-    <input v-model="message" />
-    <p>Message: {{ message }}</p>
-  </div>
-
-  <div v-show="showData"> It wil show if showData is true</div>
-  <button @click="toggleShowData">Toggle showData </button>
-
+  <!-- <OptionsAPI /> -->
+  <CompositionAPI />
 </template>
---
+++ src/components/CompositionAPI.vue
@@ -0,0 +1,90 @@
+<script setup>
+
+import { ref } from 'vue';
+
+const status = ref('Active');
+const toStatus = ref('Logging');
+
+const changeStatus = () => {
+
+    if (status.value === 'Active') {
+        status.value = 'Logging';
+        toStatus.value = 'Inactive'
+    }
+    else if (status.value === 'Logging') {
+        status.value = 'Inactive';
+        toStatus.value = 'Active';
+    }
+    else {
+        status.value = 'Active';
+        toStatus.value = 'Logging';
+    }
+
+}
+
+</script>
+
+
+<template>
+
+    <div>
+        <h1 class="font-bold">User's Status</h1>
+        <!-- Checking if else if else -->
+        <div>
+            <h1 v-if="status === 'Active'">
+                Eminem is <span class="bg-green-500 text-white"> Active </span>
+            </h1>
+            <h1 v-else-if="status === 'Logging'">
+                Eminem is <span class="bg-blue-500 text-white"> Logging </span>
+            </h1>
+            <h1 v-else>
+                Eminem is <span class="bg-red-500 text-white"> Inactive </span>
+            </h1>
+        </div>
+
+    </div>
+
+
+    <span>Change status to <button @click="changeStatus"
+            :class="status === 'Active' ? 'btn blue' : status === 'Logging' ? 'btn red' : 'btn green'"> {{ toStatus }}
+        </button> </span>
+
+
+</template>
+
+<style scoped>
+.btn {
+    padding: 2px;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+}
+
+.red {
+    background-color: red;
+    color: white;
+}
+
+.red:hover {
+    background-color: darkred;
+}
+
+.blue {
+    background-color: blue;
+    color: white;
+}
+
+.blue:hover {
+    background-color: darkblue;
+}
+
+.green {
+    background-color: green;
+    color: white;
+}
+
+.green:hover {
+    background-color: darkgreen;
+}
+</style>
