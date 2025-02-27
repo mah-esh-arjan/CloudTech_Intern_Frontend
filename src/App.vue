5b6ca47eb0f85ccc863c3496995140982c8bdf40
Mahesh maharjan <mah.dot98@gmail.com>
2025-02-27 18:22:54 +0545

    Added conditional role rendering and cart state management

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,83 @@
 <script setup>
+import { computed, ref } from 'vue';
+import { useRouter } from 'vue-router';
+import { useStore } from 'vuex';
 
+const lms = useStore();
+const router = useRouter();
+
+const role = computed(() => lms.getters.getRole);
+const user = computed(() => lms.getters.getUser);
+const count = computed(() => lms.getters.getCount);
+
+const portal = ref(false);
+
+const logoimagePath = '/images/lms.png';
+const adminImagePath = '/images/admin.png';
+const studentImagePath = 'http://127.0.0.1:8000/images';
+
+const handleProfileClick = (id) => {
+  router.push(`/student/student-details/${id}`);
+
+}
+
+const handlePortal = () => {
+  portal.value = !portal.value
+}
 
 </script>
 
 <template>
-  <div>
+  <div v-if="role === 'admin'">
     <nav class="nav">
-      <!-- ALl testing components only below: -->
-      <!-- <router-link to="/todo">Todo</router-link>
-      <router-link to="/blogs">Blogs</router-link>
-      <router-link to="/slot">Slot</router-link>
-      <router-link to="/tabs">Tabs</router-link>
-      <router-link to="/about">About</router-link> -->
-      <router-link to="/admin/student-list">Student-list</router-link>
-      <router-link to="/admin/books-list">Books</router-link>
-      <router-link to="/admin/create-book">Create-Books</router-link>
+      <div class="flex">
+        <img class="h-[45px] w-[75px] object-contain ml-4!" :src="logoimagePath" alt="Not Found" />
+        <router-link to="/admin/student-list">Student-list</router-link>
+        <router-link to="/admin/books-list">Books</router-link>
+        <router-link to="/admin/create-book">Create-Books</router-link>
+        <router-link to="/admin/admin-login">Admin-Login</router-link>
+        <router-link to="/student/student-login">Student-Login</router-link>
+      </div>
 
+      <div class="flex items-center mr-4! gap-4">
+
+        <img class="h-[45px] w-[50px] rounded-full border-1 border-white object-cover " :src="adminImagePath"
+          alt="Not Found" />
+        <h1 class="text-white">Admin</h1>
+      </div>
+
+    </nav>
+    <router-view></router-view>
+  </div>
+  <div v-else-if="role === 'student'">
+    <nav class="nav">
+      <div class="flex">
+        <img class="h-[45px] w-[75px] object-contain ml-4!" :src="logoimagePath" alt="Not Found" />
+        <router-link to="/student/student-register">Register</router-link>
+        <router-link to="/student/student-login">Student-Login</router-link>
+        <router-link to="/student/student-books-list">Student-BookList</router-link>
+        <router-link to=""><button @click="handleProfileClick(user.student_id)">Student-Details</button></router-link>
+      </div>
+
+      <div v-if="user != null" class="flex items-center mr-4! gap-4">
+        <button @click="handlePortal">POrtal</button>
+        <h1>{{ portal ? 'Active' : 'Inactive' }}</h1>
+        <h1 class="text-white"> Books: {{ count }}</h1>
+        <img class="h-[45px] w-[50px] rounded-full border-1 border-white object-cover"
+          :src="`${studentImagePath}/${user.image_path}`" alt="User Not Found" />
+        <button @click="handleProfileClick(user.student_id)" class="text-white">{{ user.name }}</button>
+      </div>
+    </nav>
+    <div :class='portal ? "frontground" : "hideground"'> <button @click="handlePortal">X</button>
+    </div>
+    <router-view></router-view>
+  </div>
+  <div v-else>
+    <nav class="nav">
+      <img class="h-[45px] w-[75px] object-contain ml-4!" :src="imagePath" alt="Not Found" />
       <router-link to="/student/student-register">Register</router-link>
       <router-link to="/student/student-login">Student-Login</router-link>
-      <router-link to="/student/student-books-list">Student-BookList</router-link>
-
-      <router-link to="/admin/admin-login" >Admin-Login</router-link>
-
-
+      <router-link to="/admin/admin-login">Admin-Login</router-link>
     </nav>
     <router-view></router-view>
   </div>
@@ -32,12 +87,11 @@
 <style scoped>
 .nav {
   display: flex;
-  gap: 45px;
-  justify-content: center;
-  background-color: #1a1a2e;
+  background-color: #2c2c4a;
   /* Dark blue background */
   padding: 15px 0;
   border-radius: 8px;
+  justify-content: space-between;
 }
 
 .nav a,
@@ -47,7 +101,7 @@
   text-decoration: none;
   font-size: 18px;
   font-weight: bold;
-  padding: 10px 15px;
+  padding: 8px 10px;
   transition: color 0.3s, background 0.3s;
   border-radius: 5px;
 }
@@ -58,5 +112,29 @@
   /* Dark text on hover */
   background: #e0e0e0;
   /* Light grey background */
+}
+
+.frontground {
+  background-color: rgba(177, 199, 177, 0.95);
+  /* Slight transparency */
+  position: fixed;
+  /* Keeps modal centered */
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 80%;
+  height: 80%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 10px;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.3);
+}
+
+.hideground {
+  visibility: hidden;
+  /* Instead of display: none, avoids rendering issues */
+  opacity: 0;
+  pointer-events: none;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -4,8 +4,10 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./views/router";
 import store from "./store/store";
+import lms from "./store/lms";
 
 const app = createApp(App);
 app.use(router);
 app.use(store);
+app.use(lms);
 app.mount("#app");
